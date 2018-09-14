ec764e8b52bfaecde64ce78c36516f62c27b08e1
Michael Insalaco <insalaco01@gmail.com>
2018-09-14 18:46:45 -0400

    create Github api class

---
+++ github.js
@@ -0,0 +1,18 @@
+class Github {
+  constructor() {
+    this.client_id     = '';
+    this.client_secret = '';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      // profile: profile
+      // if both above or the same you can use 'profile'
+      profile
+    }
+  }
+}
